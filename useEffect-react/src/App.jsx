0fda3cb5a280eb3bff71d9661ab87eb41ffe9c29
vnKhattiya <vannapars.kha@sbac.ac.th>
2025-02-10 10:59:09 +0700

    ดึงข้อมูลapi

---
+++ useEffect-react/src/App.jsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from 'react'
+
+const App = () => {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    fetch('https://dummyjson.com/products')
+      .then(res => res.json())
+      .then(item => {
+        setData(item.products);
+        console.log(item.products);
+      });
+  }, [])
+  return (
+    <div>
+      <h1>Products</h1>
+      {
+        data.map( i => {
+          return(
+            <div className="card" style={{ width: '18rem' }}>
+            <img src={i.images[0]} className="card-img-top" alt="..." />
+            <div className="card-body">
+                <h6>{i.price} THB</h6>
+                <h5 className="card-title">{i.title}</h5>
+                <p className="card-text">{i.description}</p>
+                <a href="#" className="btn btn-primary">buy</a>
+            </div>
+        </div>
+          )
+        }
+        )
+      }
+    </div>
+  )
+}
+
+export default App
