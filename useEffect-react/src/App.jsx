afe0dea3dcdf5db0c8394bc8c0f9b6c77f2f5740
vnKhattiya <vannapars.kha@sbac.ac.th>
2025-02-10 11:06:40 +0700

    -

--- useEffect-react/src/App.jsx
+++ useEffect-react/src/App.jsx
@@ -13,13 +13,15 @@ const App = () => {
   return (
     <div>
       <h1>Products</h1>
+      <div className="container">
+        <div className="d-flex row justify-content-center gap-4">
       {
         data.map( i => {
           return(
             <div className="card" style={{ width: '18rem' }}>
             <img src={i.images[0]} className="card-img-top" alt="..." />
             <div className="card-body">
-                <h6>{i.price} THB</h6>
+                <h6>{i.price} USD</h6>
                 <h5 className="card-title">{i.title}</h5>
                 <p className="card-text">{i.description}</p>
                 <a href="#" className="btn btn-primary">buy</a>
@@ -29,6 +31,8 @@ const App = () => {
         }
         )
       }
+      </div>
+      </div>
     </div>
   )
 }
